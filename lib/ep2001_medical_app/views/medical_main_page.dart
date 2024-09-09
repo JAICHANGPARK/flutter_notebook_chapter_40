ed4ded872f4b339985c52cc481409c61c03de0cd
JAICHANGPARK <aristojeff@gmail.com>
2024-09-09 20:41:37 +0900

    Create medical_main_page.dart

---
+++ lib/ep2001_medical_app/views/medical_main_page.dart
@@ -0,0 +1,15 @@
+
+
+class MedicalMainPage extends StatefulWidget {
+  const MedicalMainPage({super.key});
+
+  @override
+  State<MedicalMainPage> createState() => _MedicalMainPageState();
+}
+
+class _MedicalMainPageState extends State<MedicalMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
