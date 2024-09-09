6630fc6367e2eade276bf7ee77bd64ff66ffcc59
JAICHANGPARK <aristojeff@gmail.com>
2024-09-09 20:47:09 +0900

    Update medical_main_page.dart

--- lib/ep2001_medical_app/views/medical_main_page.dart
+++ lib/ep2001_medical_app/views/medical_main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:hugeicons/hugeicons.dart';
 
 class MedicalMainPage extends StatefulWidget {
   const MedicalMainPage({super.key});
@@ -16,7 +17,30 @@ class _MedicalMainPageState extends State<MedicalMainPage> {
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: [
-            BottomNavigationBarItem(icon: Icon())
+          BottomNavigationBarItem(
+            icon: Icon(
+              HugeIcons.strokeRoundedHome01,
+            ),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              HugeIcons.strokeRoundedDoctor01,
+            ),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              HugeIcons.strokeRoundedCalendarCheckIn01,
+            ),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              HugeIcons.strokeRoundedUser,
+            ),
+            label: "Home",
+          ),
         ],
       ),
     );
