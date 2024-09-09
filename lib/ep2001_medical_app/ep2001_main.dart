faf89ae4c0835fcfeff489fa7705f5ebc8e35314
JAICHANGPARK <aristojeff@gmail.com>
2024-09-09 20:38:42 +0900

    Update ep2001_main.dart

---
+++ lib/ep2001_medical_app/ep2001_main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+
+class MedialMobileApp extends StatelessWidget {
+  const MedialMobileApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
