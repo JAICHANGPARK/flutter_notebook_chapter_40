871764641a071cf384b76f1da0a595de390ae043
JAICHANGPARK <aristojeff@gmail.com>
2024-09-09 20:42:53 +0900

    Update ep2001_main.dart

--- lib/ep2001_medical_app/ep2001_main.dart
+++ lib/ep2001_medical_app/ep2001_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_40/ep2001_medical_app/views/medical_main_page.dart';
 
 void main() {
   runApp(MedialMobileApp());
@@ -10,7 +11,7 @@ class MedialMobileApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      home: MedicalMainPage(),
     );
   }
 }
