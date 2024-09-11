5f170f961a0a4cc1afde867fc651c3e1a3342fd4
Dreamwalker <aristojeff@gmail.com>
2024-09-12 00:35:25 +0900

    Update ep2001_main.dart

--- lib/ep2001_medical_app/ep2001_main.dart
+++ lib/ep2001_medical_app/ep2001_main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_40/ep2001_medical_app/views/medical_main_page.dart';
 
 void main() {
-  runApp(MedialMobileApp());
+  runApp(const MedialMobileApp());
 }
 
 class MedialMobileApp extends StatelessWidget {
