06afc5bed927d4fb4ec1eda4ef2e334970e51ce1
Dreamwalker <aristojeff@gmail.com>
2024-09-13 00:59:47 +0900

    Update medical_main_page.dart

--- lib/ep2001_medical_app/views/medical_main_page.dart
+++ lib/ep2001_medical_app/views/medical_main_page.dart
@@ -56,202 +56,200 @@ class _MedicalMainPageState extends State<MedicalMainPage> {
             ),
           ),
           Expanded(
-            child: Container(
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(16, 16, 0, 16),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          "Top searched specialties",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18,
-                          ),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16, 16, 0, 16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Top searched specialties",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
                         ),
-                        Gap(16),
-                        Container(
-                          // color: Colors.blue,
-                          height: 84,
-                          child: ListView(
-                            scrollDirection: Axis.horizontal,
-                            children: [
-                              Container(
-                                margin: EdgeInsets.only(right: 32),
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Icon(
-                                      HugeIcons.strokeRoundedAiPhone01,
-                                      size: 42,
-                                    ),
-                                    Text("Video visit")
-                                  ],
-                                ),
-                              ),
-                              Container(
-                                margin: EdgeInsets.only(right: 24),
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Icon(
-                                      HugeIcons.strokeRoundedDoctor01,
-                                      size: 42,
-                                    ),
-                                    Text("Dentist")
-                                  ],
-                                ),
-                              ),
-                              Container(
-                                margin: EdgeInsets.only(right: 32),
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Icon(
-                                      HugeIcons.strokeRoundedBrain02,
-                                      size: 42,
-                                    ),
-                                    Text("Psychiatrist")
-                                  ],
-                                ),
-                              ),
-                              Container(
-                                margin: EdgeInsets.only(right: 32),
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Icon(
-                                      Icons.female,
-                                      size: 42,
-                                    ),
-                                    Text("OBGYN")
-                                  ],
-                                ),
-                              ),
-                              Container(
-                                margin: EdgeInsets.only(right: 32),
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Icon(
-                                      HugeIcons.strokeRoundedDoctor01,
-                                      size: 42,
-                                    ),
-                                    Text("Dentist")
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  Divider(),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Available doctors",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () {},
-                          child: Text(
-                            "View all",
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 16,
                       ),
-                      child: GridView.builder(
-                        padding: EdgeInsets.zero,
-                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2,
-                          crossAxisSpacing: 12,
-                          mainAxisSpacing: 12,
-                          childAspectRatio: .6,
-                        ),
-                        itemBuilder: (context, index) {
-                          return Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Expanded(
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.blue[50],
-                                  ),
-                                ),
-                              ),
-                              Gap(6),
-                              Text(
-                                "Dr, Dreamwalker",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-                              Text("Flutter (Development)"),
-                              Gap(6),
-                              Row(
+                      Gap(16),
+                      Container(
+                        // color: Colors.blue,
+                        height: 84,
+                        child: ListView(
+                          scrollDirection: Axis.horizontal,
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(right: 32),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                 children: [
                                   Icon(
-                                    Icons.star,
-                                    color: Colors.orange,
-                                    size: 16,
+                                    HugeIcons.strokeRoundedAiPhone01,
+                                    size: 42,
+                                  ),
+                                  Text("Video visit")
+                                ],
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(right: 24),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Icon(
+                                    HugeIcons.strokeRoundedDoctor01,
+                                    size: 42,
+                                  ),
+                                  Text("Dentist")
+                                ],
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(right: 32),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Icon(
+                                    HugeIcons.strokeRoundedBrain02,
+                                    size: 42,
+                                  ),
+                                  Text("Psychiatrist")
+                                ],
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(right: 32),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Icon(
+                                    Icons.female,
+                                    size: 42,
+                                  ),
+                                  Text("OBGYN")
+                                ],
+                              ),
+                            ),
+                            Container(
+                              margin: EdgeInsets.only(right: 32),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Icon(
+                                    HugeIcons.strokeRoundedDoctor01,
+                                    size: 42,
+                                  ),
+                                  Text("Dentist")
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Divider(),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Available doctors",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          "View all",
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                    ),
+                    child: GridView.builder(
+                      padding: EdgeInsets.zero,
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 12,
+                        mainAxisSpacing: 12,
+                        childAspectRatio: .6,
+                      ),
+                      itemBuilder: (context, index) {
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Expanded(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.blue[50],
+                                ),
+                              ),
+                            ),
+                            Gap(6),
+                            Text(
+                              "Dr, Dreamwalker",
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Text("Flutter (Development)"),
+                            Gap(6),
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.star,
+                                  color: Colors.orange,
+                                  size: 16,
+                                ),
+                                Gap(6),
+                                Text(
+                                  "5.0 (209)",
+                                ),
+                              ],
+                            ),
+                            Gap(8),
+                            Container(
+                              height: 38,
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.blue,
+                                ),
+                                borderRadius: BorderRadius.circular(4),
+                              ),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(
+                                    Icons.video_camera_back_outlined,
+                                    size: 18,
+                                    color: Colors.blue,
                                   ),
                                   Gap(6),
                                   Text(
-                                    "5.0 (209)",
-                                  ),
-                                ],
-                              ),
-                              Gap(8),
-                              Container(
-                                height: 38,
-                                decoration: BoxDecoration(
-                                  border: Border.all(
-                                    color: Colors.blue,
-                                  ),
-                                  borderRadius: BorderRadius.circular(4),
-                                ),
-                                child: Row(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: [
-                                    Icon(
-                                      Icons.video_camera_back_outlined,
-                                      size: 18,
+                                    "Video Consult",
+                                    style: TextStyle(
                                       color: Colors.blue,
                                     ),
-                                    Gap(6),
-                                    Text(
-                                      "Video Consult",
-                                      style: TextStyle(
-                                        color: Colors.blue,
-                                      ),
-                                    )
-                                  ],
-                                ),
-                              )
-                            ],
-                          );
-                        },
-                      ),
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        );
+                      },
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ],
