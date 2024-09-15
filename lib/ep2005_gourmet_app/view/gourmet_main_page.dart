4d5ec4b005f6b498987e5d82a319c8b232d6910a
JAICHANGPARK <aristojeff@gmail.com>
2024-09-15 14:24:30 +0900

    Update gourmet_main_page.dart

---
+++ lib/ep2005_gourmet_app/view/gourmet_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class GourmetMainPage extends StatefulWidget {
+  const GourmetMainPage({super.key});
+
+  @override
+  State<GourmetMainPage> createState() => _GourmetMainPageState();
+}
+
+class _GourmetMainPageState extends State<GourmetMainPage> {
+  int menuNumber = 0;
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
