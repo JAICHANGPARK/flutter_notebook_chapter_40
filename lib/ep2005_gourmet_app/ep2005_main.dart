e9085471f9fda3e52eef2012c5e0289489e59f7c
JAICHANGPARK <aristojeff@gmail.com>
2024-09-16 11:08:47 +0900

    Update ep2005_main.dart

--- lib/ep2005_gourmet_app/ep2005_main.dart
+++ lib/ep2005_gourmet_app/ep2005_main.dart
@@ -13,17 +13,18 @@ class GourmetApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-          scaffoldBackgroundColor: const Color.fromRGBO(255, 253, 237, 1),
-          bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-            type: BottomNavigationBarType.fixed,
-            backgroundColor: Color.fromRGBO(255, 253, 237, 1),
-            selectedItemColor: Color.fromRGBO(58, 77, 57, 1),
+        scaffoldBackgroundColor: const Color.fromRGBO(255, 253, 237, 1),
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          type: BottomNavigationBarType.fixed,
+          backgroundColor: Color.fromRGBO(255, 253, 237, 1),
+          selectedItemColor: Color.fromRGBO(58, 77, 57, 1),
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            foregroundColor: const Color.fromRGBO(58, 77, 57, 1),
           ),
-          textButtonTheme: TextButtonThemeData(
-            style: TextButton.styleFrom(
-              foregroundColor: const Color.fromRGBO(58, 77, 57, 1),
-            ),
-          )),
+        ),
+      ),
       home: GourmetMainPage(),
     );
   }
