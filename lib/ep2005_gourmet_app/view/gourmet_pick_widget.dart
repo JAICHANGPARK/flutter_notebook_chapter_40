b0cb1da22b521a90eb03a81f97b08771c324b16f
JAICHANGPARK <aristojeff@gmail.com>
2024-09-18 10:08:11 +0900

    Update gourmet_pick_widget.dart

--- lib/ep2005_gourmet_app/view/gourmet_pick_widget.dart
+++ lib/ep2005_gourmet_app/view/gourmet_pick_widget.dart
@@ -6,6 +6,83 @@ class GourmetPickWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: Colors.grey[300]!,
+        ),
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Stack(
+        children: [
+          Positioned(
+            left: 8,
+            top: 8,
+            bottom: 8,
+            right: 8,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Container(),
+                ),
+                Text(
+                  "Mixed paella",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.timer_sharp,
+                      size: 16,
+                    ),
+                    Text("15 min"),
+                    Gap(12),
+                    Text("500 Kal"),
+                  ],
+                ),
+                Gap(12),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "\$30.00",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    CircleAvatar(
+                      radius: 18,
+                      backgroundColor: Colors.black,
+                      foregroundColor: Colors.white,
+                      child: Icon(Icons.add),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+          Positioned(
+            top: 8,
+            right: 8,
+            child: Row(
+              children: [
+                Icon(
+                  Icons.star,
+                  size: 16,
+                ),
+                Text(
+                  "4.5",
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
