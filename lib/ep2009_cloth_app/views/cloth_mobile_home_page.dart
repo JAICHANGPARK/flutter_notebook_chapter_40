b185795d8167ea78df95194b51023e9bdf154506
JAICHANGPARK <aristojeff@gmail.com>
2024-09-19 18:40:10 +0900

    Create cloth_mobile_home_page.dart

---
+++ lib/ep2009_cloth_app/views/cloth_mobile_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class ClothMobileHomePage extends StatefulWidget {
+  const ClothMobileHomePage({super.key});
+
+  @override
+  State<ClothMobileHomePage> createState() => _ClothMobileHomePageState();
+}
+
+class _ClothMobileHomePageState extends State<ClothMobileHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
