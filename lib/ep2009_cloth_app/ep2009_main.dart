7c3b9aa7edf0e40de3691dd161f41c530e9993ce
JAICHANGPARK <aristojeff@gmail.com>
2024-09-19 18:38:34 +0900

    Update ep2009_main.dart

---
+++ lib/ep2009_cloth_app/ep2009_main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class ClothApp extends StatelessWidget {
+  const ClothApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
