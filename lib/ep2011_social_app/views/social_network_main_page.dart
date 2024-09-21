e83d38b92fadd3a178c9884e3fb4af82c392e314
JAICHANGPARK <aristojeff@gmail.com>
2024-09-21 11:54:46 +0900

    Create social_network_main_page.dart

---
+++ lib/ep2011_social_app/views/social_network_main_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class SocialNetworkMainPage extends StatefulWidget {
+  const SocialNetworkMainPage({super.key});
+
+  @override
+  State<SocialNetworkMainPage> createState() => _SocialNetworkMainPageState();
+}
+
+class _SocialNetworkMainPageState extends State<SocialNetworkMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
