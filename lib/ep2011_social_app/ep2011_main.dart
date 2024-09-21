26f9e1893ae0806be65f4c3102d25731c9946793
JAICHANGPARK <aristojeff@gmail.com>
2024-09-21 11:53:46 +0900

    Update ep2011_main.dart

---
+++ lib/ep2011_social_app/ep2011_main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+
+class SocialNetworkApp extends StatelessWidget {
+  const SocialNetworkApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
