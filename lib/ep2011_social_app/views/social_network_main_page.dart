7eccde22f193ae4b1d63ffddc3ed928be61da2b4
JAICHANGPARK <aristojeff@gmail.com>
2024-09-21 11:54:54 +0900

    Update social_network_main_page.dart

--- lib/ep2011_social_app/views/social_network_main_page.dart
+++ lib/ep2011_social_app/views/social_network_main_page.dart
@@ -11,6 +11,8 @@ class SocialNetworkMainPage extends StatefulWidget {
 class _SocialNetworkMainPageState extends State<SocialNetworkMainPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      
+    );
   }
 }
