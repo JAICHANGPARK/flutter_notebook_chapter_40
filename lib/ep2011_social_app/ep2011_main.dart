6d6272942ada997a24b99b8ba65abffac987b4a4
JAICHANGPARK <aristojeff@gmail.com>
2024-09-21 11:56:13 +0900

    Update ep2011_main.dart

--- lib/ep2011_social_app/ep2011_main.dart
+++ lib/ep2011_social_app/ep2011_main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_notebook_chapter_40/ep2011_social_app/views/social_network_main_page.dart';
 
 void main() {
   runApp(SocialNetworkApp());
@@ -11,7 +12,8 @@ class SocialNetworkApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      
+      home: SocialNetworkMainPage(),
+
     );
   }
 }
