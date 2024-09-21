3bad7060e5cfded5e46d9c29292643141446e2c7
JAICHANGPARK <aristojeff@gmail.com>
2024-09-21 12:01:58 +0900

    Update social_network_main_page.dart

--- lib/ep2011_social_app/views/social_network_main_page.dart
+++ lib/ep2011_social_app/views/social_network_main_page.dart
@@ -15,39 +15,48 @@ class _SocialNetworkMainPageState extends State<SocialNetworkMainPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Row(
-              children: [
-                CircleAvatar(),
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text("STATUS"),
-                      Row(
-                        children: [
-                          Text("Up for coffee"),
-                          Icon(
-                            Icons.keyboard_arrow_down,
-                            size: 16,
-                          ),
-                        ],
-                      ),
-                    ],
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  CircleAvatar(),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("STATUS"),
+                        Row(
+                          children: [
+                            Text("Up for coffee"),
+                            Icon(
+                              Icons.keyboard_arrow_down,
+                              size: 16,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-                Container(
-                  decoration: BoxDecoration(),
-                  child: Text("3 Requests"),
-                ),
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.filter_list),
-                ),
+                  Container(
+                    decoration: BoxDecoration(),
+                    child: Text("3 Requests"),
+                  ),
+                  IconButton(
+                    onPressed: () {},
+                    icon: Icon(Icons.filter_list),
+                  ),
+                ],
+              ),
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Human"),
+                SizedBox(height: 140,
+                  child: Placeholder(),)
               ],
             ),
-            Text("Human"),
-            SizedBox(height: 180,
-            child: Placeholder(),)
+
           ],
         ),
       ),
