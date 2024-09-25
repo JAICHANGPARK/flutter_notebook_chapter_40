11aeb87691949066cbf902c1fb21ac247b8677b2
JAICHANGPARK <aristojeff@gmail.com>
2024-09-25 19:17:08 +0900

    Create flight_booking_home_page.dart

---
+++ lib/ep2015_flight_booking_app/views/flight_booking_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class FlightBookingHomePage extends StatefulWidget {
+  const FlightBookingHomePage({super.key});
+
+  @override
+  State<FlightBookingHomePage> createState() => _FlightBookingHomePageState();
+}
+
+class _FlightBookingHomePageState extends State<FlightBookingHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
