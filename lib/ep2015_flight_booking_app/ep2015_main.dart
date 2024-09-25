aaca9d76cadde8ad0f03fc5f500010f7c47a2a5f
JAICHANGPARK <aristojeff@gmail.com>
2024-09-25 19:15:21 +0900

    Update ep2015_main.dart

--- lib/ep2015_flight_booking_app/ep2015_main.dart
+++ lib/ep2015_flight_booking_app/ep2015_main.dart
@@ -1,2 +1,11 @@
 import 'package:flutter/material.dart';
 
+
+class FlightBookingApp extends StatelessWidget {
+  const FlightBookingApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
