618c58c001a9001487890c5cbdd697c25da3a60f
JAICHANGPARK <aristojeff@gmail.com>
2024-09-25 19:21:43 +0900

    Update flight_booking_home_page.dart

--- lib/ep2015_flight_booking_app/views/flight_booking_home_page.dart
+++ lib/ep2015_flight_booking_app/views/flight_booking_home_page.dart
@@ -22,7 +22,7 @@ class _FlightBookingHomePageState extends State<FlightBookingHomePage> {
                   end: Alignment.topCenter,
                   colors: [
                     Color.fromRGBO(206, 194, 185, 1),
-                    Color.fromRGBO(206, 194, 185, 1),
+                    Color.fromRGBO(195, 187, 181, 1),
                     Color.fromRGBO(175, 174, 171, 1),
                     Color.fromRGBO(156, 162, 165, 1),
                     Color.fromRGBO(146, 161, 169, 1),
