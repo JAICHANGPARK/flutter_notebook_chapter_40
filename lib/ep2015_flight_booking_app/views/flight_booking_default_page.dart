787d95cf7550486616f0523a214e6b35b2540460
JAICHANGPARK <aristojeff@gmail.com>
2024-09-27 13:51:14 +0900

    Update flight_booking_default_page.dart

--- lib/ep2015_flight_booking_app/views/flight_booking_default_page.dart
+++ lib/ep2015_flight_booking_app/views/flight_booking_default_page.dart
@@ -10,6 +10,34 @@ class FlightBookingDefaultPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                gradient: LinearGradient(
+                  begin: Alignment.bottomCenter,
+                  end: Alignment.topCenter,
+                  colors: [
+                    Color.fromRGBO(206, 194, 185, 1),
+                    Color.fromRGBO(195, 187, 181, 1),
+                    Color.fromRGBO(175, 174, 171, 1),
+                    Color.fromRGBO(156, 162, 165, 1),
+                    Color.fromRGBO(146, 161, 169, 1),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Positioned.fill(
+            child: SafeArea(
+              child: child,
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
