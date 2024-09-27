2d0c3b55332f47b5e26f3732cd2e0cb60e249ab3
JAICHANGPARK <aristojeff@gmail.com>
2024-09-27 13:50:18 +0900

    Update flight_booking_page.dart

---
+++ lib/ep2015_flight_booking_app/views/flight_booking_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class FlightBookingPage extends StatefulWidget {
+  const FlightBookingPage({super.key});
+
+  @override
+  State<FlightBookingPage> createState() => _FlightBookingPageState();
+}
+
+class _FlightBookingPageState extends State<FlightBookingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
