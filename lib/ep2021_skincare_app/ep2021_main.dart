7fcc7c4d985b71cecc4b6f925ed4fc5b0f5c4158
JAICHANGPARK <aristojeff@gmail.com>
2024-09-27 15:19:09 +0900

    Create ep2021_main.dart

---
+++ lib/ep2021_skincare_app/ep2021_main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  
+}
+
+class SkinCareApp extends StatelessWidget {
+  const SkinCareApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
