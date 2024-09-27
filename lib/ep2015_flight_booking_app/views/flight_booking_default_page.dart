cd0f81a81f1a7c88fef8ba1bdf582e0ad90a79af
JAICHANGPARK <aristojeff@gmail.com>
2024-09-27 13:50:20 +0900

    Create flight_booking_default_page.dart

---
+++ lib/ep2015_flight_booking_app/views/flight_booking_default_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FlightBookingDefaultPage extends StatelessWidget {
+  final Widget child;
+
+  const FlightBookingDefaultPage({
+    super.key,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
