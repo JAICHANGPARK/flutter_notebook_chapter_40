87b59d1c1a6d77805cc2b08d740afc8ac4ca1c24
JAICHANGPARK <aristojeff@gmail.com>
2024-09-27 15:26:29 +0900

    Update skincare_home_page.dart

--- lib/ep2021_skincare_app/views/skincare_home_page.dart
+++ lib/ep2021_skincare_app/views/skincare_home_page.dart
@@ -11,15 +11,21 @@ class _SkincareHomePageState extends State<SkincareHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color.fromRGBO(234, 239, 242, 1),
       body: Stack(
         children: [
           Positioned.fill(
+            left: 24,
+            right: 24,
+            top: 24,
             child: SafeArea(
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Row(
                     children: [
                       Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
                           Text(
                             "Hi Dream👋",
@@ -30,13 +36,25 @@ class _SkincareHomePageState extends State<SkincareHomePage> {
                         ],
                       ),
                       Container(
-                        height: 72,
-                        width: 72,
+                        height: 64,
+                        width: 64,
                         decoration: BoxDecoration(
                           color: Colors.blue,
                         ),
                       )
                     ],
+                  ),
+
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(32),
+                          topRight: Radius.circular(32),
+                        ),
+                      ),
+                    ),
                   )
                 ],
               ),
