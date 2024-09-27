91751086dcd6ab6d7070d1870739a20a4f61082f
JAICHANGPARK <aristojeff@gmail.com>
2024-09-27 15:21:33 +0900

    Create skincare_home_page.dart

---
+++ lib/ep2021_skincare_app/views/skincare_home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class SkincareHomePage extends StatefulWidget {
+  const SkincareHomePage({super.key});
+
+  @override
+  State<SkincareHomePage> createState() => _SkincareHomePageState();
+}
+
+class _SkincareHomePageState extends State<SkincareHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(),
+    );
+  }
+}
