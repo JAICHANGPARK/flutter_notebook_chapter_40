fe951a1e8976baf7628b51f2f2fb1e703d4297bf
JAICHANGPARK <aristojeff@gmail.com>
2024-09-27 15:24:42 +0900

    Update skincare_home_page.dart

--- lib/ep2021_skincare_app/views/skincare_home_page.dart
+++ lib/ep2021_skincare_app/views/skincare_home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class SkincareHomePage extends StatefulWidget {
   const SkincareHomePage({super.key});
 
@@ -12,7 +11,39 @@ class _SkincareHomePageState extends State<SkincareHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(),
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: SafeArea(
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Column(
+                        children: [
+                          Text(
+                            "Hi Dream👋",
+                          ),
+                          Text(
+                            "Elevate your complexion care",
+                          )
+                        ],
+                      ),
+                      Container(
+                        height: 72,
+                        width: 72,
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
