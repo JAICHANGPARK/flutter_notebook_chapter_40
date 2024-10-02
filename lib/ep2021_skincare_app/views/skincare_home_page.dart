8bee871e116dbb3641fc4165b10e5489d2896faa
JAICHANGPARK <aristojeff@gmail.com>
2024-10-02 19:08:20 +0900

    Update skincare_home_page.dart

--- lib/ep2021_skincare_app/views/skincare_home_page.dart
+++ lib/ep2021_skincare_app/views/skincare_home_page.dart
@@ -96,6 +96,7 @@ class _SkincareHomePageState extends State<SkincareHomePage> {
                                   "Use AI to scan your face",
                                   style: TextStyle(
                                     color: Colors.white,
+                                    fontSize: 16,
                                   ),
                                 ),
                                 Spacer(),
