b26054d297b1f1d79cc00c8b6d3d8458d37e33ba
JAICHANGPARK <aristojeff@gmail.com>
2024-10-03 09:42:44 +0900

    Create skincare_detail_page.dart

---
+++ lib/ep2021_skincare_app/views/skincare_detail_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SkincareDetailPage extends StatefulWidget {
+  const SkincareDetailPage({super.key});
+
+  @override
+  State<SkincareDetailPage> createState() => _SkincareDetailPageState();
+}
+
+class _SkincareDetailPageState extends State<SkincareDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
