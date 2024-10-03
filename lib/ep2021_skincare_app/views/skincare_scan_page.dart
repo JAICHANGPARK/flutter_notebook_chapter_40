41d21df8170d10a75d79ee9dfa796f51023a2e79
JAICHANGPARK <aristojeff@gmail.com>
2024-10-03 09:46:01 +0900

    Update skincare_scan_page.dart

---
+++ lib/ep2021_skincare_app/views/skincare_scan_page.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class SkincareScanPage extends StatefulWidget {
+  const SkincareScanPage({super.key});
+
+  @override
+  State<SkincareScanPage> createState() => _SkincareScanPageState();
+}
+
+class _SkincareScanPageState extends State<SkincareScanPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.network(
+              "https://cdn.pixabay.com/photo/2024/05/31/10/48/woman-8800324_1280.jpg",
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
