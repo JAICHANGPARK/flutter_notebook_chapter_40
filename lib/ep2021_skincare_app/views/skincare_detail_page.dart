430891a8ea82bf1a1145badde5a6dc8eccb8c6f5
JAICHANGPARK <aristojeff@gmail.com>
2024-10-04 18:38:11 +0900

    Update skincare_detail_page.dart

--- lib/ep2021_skincare_app/views/skincare_detail_page.dart
+++ lib/ep2021_skincare_app/views/skincare_detail_page.dart
@@ -12,60 +12,62 @@ class _SkincareDetailPageState extends State<SkincareDetailPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              children: [
-                InkWell(
-                  onTap: (){
-                    Navigator.pop(context);
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    padding: EdgeInsets.all(8),
-                    child: Icon(
-                      Icons.keyboard_arrow_left,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Center(
-                      child: Text(
-                    "Results",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                    ),
-                  )),
-                ),
-                InkWell(
-                  onTap: (){
-                    Navigator.pop(context);
-                    Navigator.pop(context);
-                  },
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    padding: EdgeInsets.all(8),
-                    child: Icon(
-                      Icons.close,
-                      color: Colors.white,
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                children: [
+                  InkWell(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      padding: EdgeInsets.all(8),
+                      child: Icon(
+                        Icons.keyboard_arrow_left,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
-                ),
-              ],
+                  Expanded(
+                    child: Center(
+                        child: Text(
+                      "Results",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16,
+                      ),
+                    )),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.pop(context);
+                      Navigator.pop(context);
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      padding: EdgeInsets.all(8),
+                      child: Icon(
+                        Icons.close,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
-          )
-        ],
-      )),
+            Text("The analysis of your skin is complete✅")
+          ],
+        ),
+      ),
     );
   }
 }
