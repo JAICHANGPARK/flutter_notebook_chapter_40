46b7b99910361339974b357b48738ee4084b7b3d
JAICHANGPARK <aristojeff@gmail.com>
2024-10-07 17:08:46 +0900

    Create estate_feed_page.dart

---
+++ lib/ep2027_estate_app/views/estate_feed_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class EstateFeedPage extends StatefulWidget {
+  const EstateFeedPage({super.key});
+
+  @override
+  State<EstateFeedPage> createState() => _EstateFeedPageState();
+}
+
+class _EstateFeedPageState extends State<EstateFeedPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
