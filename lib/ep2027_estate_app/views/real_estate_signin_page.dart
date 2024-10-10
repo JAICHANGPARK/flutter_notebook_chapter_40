152c690e785848e99f285fcbe8421ea87cddecee
JAICHANGPARK <aristojeff@gmail.com>
2024-10-10 18:32:21 +0900

    Create real_estate_signin_page.dart

---
+++ lib/ep2027_estate_app/views/real_estate_signin_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RealEstateSigninPage extends StatefulWidget {
+  const RealEstateSigninPage({super.key});
+
+  @override
+  State<RealEstateSigninPage> createState() => _RealEstateSigninPageState();
+}
+
+class _RealEstateSigninPageState extends State<RealEstateSigninPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+    );
+  }
+}
