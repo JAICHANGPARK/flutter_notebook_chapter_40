20dbf93e0f17b78fb6f32dddf0d8c1ccb10e4f56
JAICHANGPARK <aristojeff@gmail.com>
2024-10-11 19:39:51 +0900

    Update laundry_home_page.dart

--- lib/ep2030_laundry_app/views/laundry_home_page.dart
+++ lib/ep2030_laundry_app/views/laundry_home_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class LaundryHomePage extends StatefulWidget {
   const LaundryHomePage({super.key});
 
@@ -13,7 +12,25 @@ class _LaundryHomePageState extends State<LaundryHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      
+      body: Stack(
+        children: [
+          Positioned(
+              child: Column(
+            children: [
+              Container(
+                height: 240,
+                color: Colors.blue,
+              ),
+              Expanded(
+                  child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                ),
+              ))
+            ],
+          ))
+        ],
+      ),
     );
   }
 }
