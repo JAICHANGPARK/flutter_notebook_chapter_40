66f69b7bfdaf0a010c593a6c8e043deaede82a31
JAICHANGPARK <aristojeff@gmail.com>
2024-10-11 19:38:16 +0900

    Create laundry_home_page.dart

---
+++ lib/ep2030_laundry_app/views/laundry_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class LaundryHomePage extends StatefulWidget {
+  const LaundryHomePage({super.key});
+
+  @override
+  State<LaundryHomePage> createState() => _LaundryHomePageState();
+}
+
+class _LaundryHomePageState extends State<LaundryHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
