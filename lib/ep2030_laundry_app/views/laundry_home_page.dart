2ae092882c63867760e41e1610265f9904c4c8aa
JAICHANGPARK <aristojeff@gmail.com>
2024-10-11 19:41:58 +0900

    Update laundry_home_page.dart

--- lib/ep2030_laundry_app/views/laundry_home_page.dart
+++ lib/ep2030_laundry_app/views/laundry_home_page.dart
@@ -20,8 +20,7 @@ class _LaundryHomePageState extends State<LaundryHomePage> {
               Container(
                 height: 240,
                 color: Colors.blue,
-                padding: EdgeInsets.only(top: 62,
-                left: 24, right: 24),
+                padding: EdgeInsets.only(top: 62, left: 24, right: 24),
                 child: Column(
                   children: [
                     Row(
@@ -39,7 +38,25 @@ class _LaundryHomePageState extends State<LaundryHomePage> {
                               color: Colors.orange,
                             ),
                           ),
-                        )
+                        ),
+                        Expanded(
+                          child: Container(),
+                        ),
+                        Container(
+                            height: 52,
+                            width: 52,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(4),
+                            ),
+                            padding: EdgeInsets.all(3),
+                            child: Center(
+                              child: Badge(
+                                child: Icon(
+                                  Icons.notifications_active_outlined,
+                                ),
+                              ),
+                            )),
                       ],
                     )
                   ],
