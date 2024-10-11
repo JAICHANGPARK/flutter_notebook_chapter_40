dcfba5da132dfdda1381c1078eca2ab048019b55
JAICHANGPARK <aristojeff@gmail.com>
2024-10-11 19:37:09 +0900

    Update ep2030_main.dart

--- lib/ep2030_laundry_app/ep2030_main.dart
+++ lib/ep2030_laundry_app/ep2030_main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(LaundryApp());
+}
 
 class LaundryApp extends StatelessWidget {
   const LaundryApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp();
   }
 }
