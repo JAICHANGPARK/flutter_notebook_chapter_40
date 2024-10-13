a6cccafba35a4f5d712b4fbfa1cb72782010b01c
Dreamwalker <aristojeff@gmail.com>
2024-10-13 09:35:39 +0900

    Update laundry_home_page.dart

--- lib/ep2030_laundry_app/views/laundry_home_page.dart
+++ lib/ep2030_laundry_app/views/laundry_home_page.dart
@@ -16,209 +16,210 @@ class _LaundryHomePageState extends State<LaundryHomePage> {
       body: Stack(
         children: [
           Positioned(
-              child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                height: 280,
-                color: Colors.blue,
-                padding: EdgeInsets.only(
-                  top: 72,
-                  left: 24,
-                  right: 24,
-                ),
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Container(
-                          height: 48,
-                          width: 48,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(4),
-                          ),
-                          padding: EdgeInsets.all(3),
-                          child: Container(
-                            decoration: BoxDecoration(
-                              color: Colors.orange,
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          child: Container(
-                            margin: EdgeInsets.symmetric(horizontal: 42),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  height: 280,
+                  color: Colors.blue,
+                  padding: EdgeInsets.only(
+                    top: 72,
+                    left: 24,
+                    right: 24,
+                  ),
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Container(
+                            height: 48,
+                            width: 48,
                             decoration: BoxDecoration(
                               color: Colors.white,
                               borderRadius: BorderRadius.circular(4),
                             ),
-                            padding: EdgeInsets.symmetric(
-                              vertical: 12,
-                              horizontal: 12,
-                            ),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Icon(
-                                  Icons.location_on_rounded,
-                                  color: Colors.blue,
-                                  size: 20,
-                                ),
-                                Gap(8),
-                                Text(
-                                  "Seoul, Korea",
-                                  style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                Gap(8),
-                                Icon(
-                                  Icons.keyboard_arrow_down,
-                                )
-                              ],
-                            ),
-                          ),
-                        ),
-                        Container(
-                          height: 48,
-                          width: 48,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(4),
-                          ),
-                          padding: EdgeInsets.all(3),
-                          child: Center(
-                            child: Badge(
-                              child: Icon(
-                                Icons.notifications_active_outlined,
+                            padding: EdgeInsets.all(3),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.orange,
                               ),
                             ),
                           ),
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-              Expanded(
-                child: Container(
-                  padding: EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        decoration: BoxDecoration(
-                          color: Colors.grey[200],
-                        ),
-                        height: 64,
-                        child: Row(
-                          children: [
-                            Expanded(
-                              child: TextField(
-                                decoration: InputDecoration(
-                                  icon: Icon(Icons.search),
-                                  hintText: "What to wash today?",
-                                  border: InputBorder.none,
-                                ),
-                              ),
-                            ),
-                            Container(
+                          Expanded(
+                            child: Container(
+                              margin: EdgeInsets.symmetric(horizontal: 42),
                               decoration: BoxDecoration(
                                 color: Colors.white,
+                                borderRadius: BorderRadius.circular(4),
                               ),
-                              child: Icon(Icons.mic),
-                            )
-                          ],
+                              padding: EdgeInsets.symmetric(
+                                vertical: 12,
+                                horizontal: 12,
+                              ),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                mainAxisSize: MainAxisSize.min,
+                                children: [
+                                  Icon(
+                                    Icons.location_on_rounded,
+                                    color: Colors.blue,
+                                    size: 20,
+                                  ),
+                                  Gap(8),
+                                  Text(
+                                    "Seoul, Korea",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Gap(8),
+                                  Icon(
+                                    Icons.keyboard_arrow_down,
+                                  )
+                                ],
+                              ),
+                            ),
+                          ),
+                          Container(
+                            height: 48,
+                            width: 48,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(4),
+                            ),
+                            padding: EdgeInsets.all(3),
+                            child: Center(
+                              child: Badge(
+                                child: Icon(
+                                  Icons.notifications_active_outlined,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(16),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.grey[200],
+                          ),
+                          height: 64,
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    icon: Icon(Icons.search),
+                                    hintText: "What to wash today?",
+                                    border: InputBorder.none,
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                ),
+                                child: Icon(Icons.mic),
+                              )
+                            ],
+                          ),
                         ),
-                      ),
-                      Gap(12),
-                      Container(
-                        height: 120,
-                        // margin: EdgeInsets.symmetric(vertical: 8),
-                        child: Row(
-                          children: [
-                            Expanded(
-                                child: Container(
-                              decoration: BoxDecoration(
-                                  color: Colors.grey[100],
-                                  border: Border.all(
-                                    color: Colors.grey[300]!,
-                                  )),
-                            )),
-                            Gap(12),
-                            Expanded(
-                                child: Container(
-                              decoration: BoxDecoration(
-                                  color: Colors.grey[100],
-                                  border: Border.all(
-                                    color: Colors.grey[300]!,
-                                  )),
-                            )),
-                            Gap(12),
-                            Expanded(
-                              child: Container(
+                        Gap(12),
+                        Container(
+                          height: 120,
+                          // margin: EdgeInsets.symmetric(vertical: 8),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                  child: Container(
                                 decoration: BoxDecoration(
                                     color: Colors.grey[100],
                                     border: Border.all(
                                       color: Colors.grey[300]!,
                                     )),
-                              ),
-                            ),
-                            Gap(12),
-                            Expanded(
-                              child: Container(
+                              )),
+                              Gap(12),
+                              Expanded(
+                                  child: Container(
                                 decoration: BoxDecoration(
-                                  color: Colors.grey[100],
-                                  border: Border.all(
-                                    color: Colors.grey[300]!,
+                                    color: Colors.grey[100],
+                                    border: Border.all(
+                                      color: Colors.grey[300]!,
+                                    )),
+                              )),
+                              Gap(12),
+                              Expanded(
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      color: Colors.grey[100],
+                                      border: Border.all(
+                                        color: Colors.grey[300]!,
+                                      )),
+                                ),
+                              ),
+                              Gap(12),
+                              Expanded(
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey[100],
+                                    border: Border.all(
+                                      color: Colors.grey[300]!,
+                                    ),
                                   ),
                                 ),
+                              )
+                            ],
+                          ),
+                        ),
+                        Gap(12),
+                        Text("Your order is being processed"),
+                        Gap(12),
+                        Container(
+                          height: 160,
+                          child: Placeholder(),
+                        ),
+                        Gap(12),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text("Recommendations for you"),
+                            Container(
+                              child: Icon(
+                                Icons.arrow_forward,
+                                size: 16,
                               ),
                             )
                           ],
                         ),
-                      ),
-                      Gap(12),
-                      Text("Your order is being processed"),
-                      Gap(12),
-                      Container(
-                        height: 160,
-                        child: Placeholder(),
-                      ),
-                      Gap(12),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text("Recommendations for you"),
-                          Container(
-                            child: Icon(
-                              Icons.arrow_forward,
-                              size: 16,
+                        Expanded(
+                          child: GridView.builder(
+                            gridDelegate:
+                                SliverGridDelegateWithFixedCrossAxisCount(
+                              crossAxisCount: 2,
                             ),
-                          )
-                        ],
-                      ),
-                      Expanded(
-                        child: GridView.builder(
-                          gridDelegate:
-                              SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 2,
+                            itemBuilder: (context, index) {
+                              return Placeholder();
+                            },
                           ),
-                          itemBuilder: (context, index) {
-                            return Placeholder();
-                          },
-                        ),
-                      )
-                    ],
+                        )
+                      ],
+                    ),
                   ),
-                ),
-              )
-            ],
-          ))
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
