34bbd168f8164e72f9ab5477b92037824c7b51c3
JAICHANGPARK <aristojeff@gmail.com>
2024-10-15 18:46:03 +0900

    Update laundry_detail_page.dart

--- lib/ep2030_laundry_app/views/laundry_detail_page.dart
+++ lib/ep2030_laundry_app/views/laundry_detail_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class LaundryDetailPage extends StatefulWidget {
@@ -11,7 +13,22 @@ class _LaundryDetailPageState extends State<LaundryDetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Container(
+              color: Colors.white,
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 4, sigmaY: 6),
+                child: Image.network(
+                  "https://cdn.pixabay.com/photo/2021/12/29/08/30/real-estate-6900973_1280.jpg",
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
