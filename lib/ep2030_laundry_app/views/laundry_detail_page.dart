16d0cb3f51c8d6be0e4075be19dfa96ae9298d51
JAICHANGPARK <aristojeff@gmail.com>
2024-10-15 18:39:07 +0900

    Update laundry_detail_page.dart

--- lib/ep2030_laundry_app/views/laundry_detail_page.dart
+++ lib/ep2030_laundry_app/views/laundry_detail_page.dart
@@ -1,2 +1,17 @@
 import 'package:flutter/material.dart';
 
+class LaundryDetailPage extends StatefulWidget {
+  const LaundryDetailPage({super.key});
+
+  @override
+  State<LaundryDetailPage> createState() => _LaundryDetailPageState();
+}
+
+class _LaundryDetailPageState extends State<LaundryDetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
