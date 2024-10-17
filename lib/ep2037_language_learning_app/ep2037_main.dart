647428ae14239aa8366bb22aa2a17e7df827ae9e
JAICHANGPARK <aristojeff@gmail.com>
2024-10-17 18:36:18 +0900

    Update ep2037_main.dart

---
+++ lib/ep2037_language_learning_app/ep2037_main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class LanguageLearningApp extends StatelessWidget {
+  const LanguageLearningApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
