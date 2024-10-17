ccacbaa1e0622dc58daf70ff88baba75fc48e0cf
JAICHANGPARK <aristojeff@gmail.com>
2024-10-17 18:37:25 +0900

    Create language_home_page.dart

---
+++ lib/ep2037_language_learning_app/views/language_home_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+
+class LanguageHomePage extends StatefulWidget {
+  const LanguageHomePage({super.key});
+
+  @override
+  State<LanguageHomePage> createState() => _LanguageHomePageState();
+}
+
+class _LanguageHomePageState extends State<LanguageHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
