d11eb1e54919a09c4978a6b0ed89be4072741911
JAICHANGPARK <aristojeff@gmail.com>
2024-10-17 18:36:51 +0900

    Update ep2037_main.dart

--- lib/ep2037_language_learning_app/ep2037_main.dart
+++ lib/ep2037_language_learning_app/ep2037_main.dart
@@ -9,6 +9,8 @@ class LanguageLearningApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: ,
+    );
   }
 }
