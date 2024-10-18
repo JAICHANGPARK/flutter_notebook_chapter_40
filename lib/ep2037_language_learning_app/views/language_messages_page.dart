66e7d03a8d3bed4b1ed37649a237b9008a66dba0
JAICHANGPARK <aristojeff@gmail.com>
2024-10-19 08:54:49 +0900

    Update language_messages_page.dart

--- lib/ep2037_language_learning_app/views/language_messages_page.dart
+++ lib/ep2037_language_learning_app/views/language_messages_page.dart
@@ -1,3 +1,15 @@
 import 'package:flutter/material.dart';
 
-st
+class LanguageMessagesPage extends StatefulWidget {
+  const LanguageMessagesPage({super.key});
+
+  @override
+  State<LanguageMessagesPage> createState() => _LanguageMessagesPageState();
+}
+
+class _LanguageMessagesPageState extends State<LanguageMessagesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
