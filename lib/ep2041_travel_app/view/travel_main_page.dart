e5c3261c5155ed14a735d8c11853c9d35b77063a
JAICHANGPARK <aristojeff@gmail.com>
2024-10-21 16:49:37 +0900

    Update travel_main_page.dart

--- lib/ep2041_travel_app/view/travel_main_page.dart
+++ lib/ep2041_travel_app/view/travel_main_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class TravelMainPage extends StatefulWidget {
   const TravelMainPage({super.key});
 
@@ -14,7 +13,29 @@ class _TravelMainPageState extends State<TravelMainPage> {
     return Scaffold(
       body: Stack(
         children: [
-          Positioned(child: Image.network("https://cdn.pixabay.com/photo/2023/03/19/19/38/mountain-7863224_1280.jpg"))
+          Positioned.fill(
+            child: Image.network(
+              "https://cdn.pixabay.com/photo/2023/03/19/19/38/mountain-7863224_1280.jpg",
+              fit: BoxFit.cover,
+            ),
+          ),
+          Positioned(
+            left: 16,
+            right: 16,
+            bottom: 0,
+            top: 24,
+            child: SafeArea(
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      
+                    ],
+                  )
+                ],
+              ),
+            ),
+          )
         ],
       ),
     );
