e10e44fc51808dfeb4adbb4dda5046328e741a8b
JAICHANGPARK <aristojeff@gmail.com>
2024-10-23 19:26:27 +0900

    Update ep2041_main.dart

--- lib/ep2041_travel_app/ep2041_main.dart
+++ lib/ep2041_travel_app/ep2041_main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_40/ep2041_travel_app/view/travel_main_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(TravelApp());
@@ -11,6 +12,9 @@ class TravelApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        fontFamily: GoogleFonts.jimNightshade().fontFamily,
+      ),
       home: TravelMainPage(),
     );
   }
