3eeb698b81d355cbf01b7918d5e4d55f17d98b71
JAICHANGPARK <aristojeff@gmail.com>
2024-10-26 09:37:44 +0900

    Update travel_home_page.dart

--- lib/ep2041_travel_app/view/travel_home_page.dart
+++ lib/ep2041_travel_app/view/travel_home_page.dart
@@ -311,6 +311,7 @@ class _TravelHomePageState extends State<TravelHomePage> {
                                               child: Container(
                                                 decoration: BoxDecoration(
                                                   color: Colors.red,
+                                                  borderRadius: BorderRadius.circular(7),
                                                 ),
                                               ),
                                             ),
