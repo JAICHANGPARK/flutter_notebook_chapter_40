95aadb9ec33a867118861dc0392bf0bedd2a454a
JAICHANGPARK <aristojeff@gmail.com>
2024-10-26 09:07:12 +0900

    Update ep2041_main.dart

--- lib/ep2041_travel_app/ep2041_main.dart
+++ lib/ep2041_travel_app/ep2041_main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_notebook_chapter_40/ep2041_travel_app/view/travel_home_page.dart';
-import 'package:flutter_notebook_chapter_40/ep2041_travel_app/view/travel_main_page.dart';
-import 'package:google_fonts/google_fonts.dart';
+
 
 void main() {
   runApp(TravelApp());
