94d4b8839aaf0e824b4748ed9a8542c464fd5e71
JAICHANGPARK <aristojeff@gmail.com>
2024-10-27 09:41:09 +0900

    Update sales_analytics_main_page.dart

--- lib/ep2047_sales_analytics_app/views/sales_analytics_main_page.dart
+++ lib/ep2047_sales_analytics_app/views/sales_analytics_main_page.dart
@@ -18,86 +18,92 @@ class _SalesAnalyticsMainPageState extends State<SalesAnalyticsMainPage> {
           children: [
             Padding(
               padding: const EdgeInsets.all(16.0),
-              child: Row(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.grey[300]!,
-                      ),
-                      borderRadius: BorderRadius.circular(4),
-                    ),
-                    padding: EdgeInsets.all(6),
-                    child: Icon(
-                      Icons.menu,
-                      size: 20,
-                    ),
-                  ),
-                  Spacer(),
-                  Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.grey[300]!,
-                      ),
-                      borderRadius: BorderRadius.circular(4),
-                    ),
-                    padding: EdgeInsets.all(6),
-                    child: Icon(
-                      Icons.mail_outline,
-                      size: 20,
-                    ),
-                  ),
-                  Gap(12),
-                  Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.grey[300]!,
-                      ),
-                      borderRadius: BorderRadius.circular(4),
-                    ),
-                    padding: EdgeInsets.all(6),
-                    child: Icon(
-                      Icons.notifications_none,
-                      size: 20,
-                    ),
-                  ),
-                  VerticalDivider(),
-                  Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.grey[300]!,
-                      ),
-                      borderRadius: BorderRadius.circular(4),
-                    ),
-                    padding: EdgeInsets.all(6),
-                    child: Row(
-                      children: [
-                        CircleAvatar(
-                          radius: 10,
+              child: IntrinsicHeight(
+                child: Row(
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.grey[300]!,
                         ),
-                        Gap(8),
-                        Icon(
-                          Icons.keyboard_arrow_down,
-                          size: 20,
-                        ),
-                      ],
-                    ),
-                  ),
-                  Gap(12),
-                  Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.grey[300]!,
+                        borderRadius: BorderRadius.circular(4),
                       ),
-                      borderRadius: BorderRadius.circular(4),
+                      padding: EdgeInsets.all(6),
+                      child: Icon(
+                        Icons.menu,
+                        size: 20,
+                      ),
                     ),
-                    padding: EdgeInsets.all(6),
-                    child: Icon(
-                      Icons.more_horiz,
-                      size: 20,
+                    Spacer(),
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.grey[300]!,
+                        ),
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      padding: EdgeInsets.all(6),
+                      child: Icon(
+                        Icons.mail_outline,
+                        size: 20,
+                      ),
                     ),
-                  ),
-                ],
+                    Gap(12),
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.grey[300]!,
+                        ),
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      padding: EdgeInsets.all(6),
+                      child: Icon(
+                        Icons.notifications_none,
+                        size: 20,
+                      ),
+                    ),
+                    VerticalDivider(
+                      width: 24,
+                      endIndent: 4,
+                      indent: 4,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.grey[300]!,
+                        ),
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      padding: EdgeInsets.all(6),
+                      child: Row(
+                        children: [
+                          CircleAvatar(
+                            radius: 10,
+                          ),
+                          Gap(8),
+                          Icon(
+                            Icons.keyboard_arrow_down,
+                            size: 20,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Gap(12),
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.grey[300]!,
+                        ),
+                        borderRadius: BorderRadius.circular(4),
+                      ),
+                      padding: EdgeInsets.all(6),
+                      child: Icon(
+                        Icons.more_horiz,
+                        size: 20,
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
             Divider(),
