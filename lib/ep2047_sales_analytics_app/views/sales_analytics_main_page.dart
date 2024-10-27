9a24b23fd23df79a7a233f537fcb4d387738be6f
JAICHANGPARK <aristojeff@gmail.com>
2024-10-27 09:34:09 +0900

    Create sales_analytics_main_page.dart

---
+++ lib/ep2047_sales_analytics_app/views/sales_analytics_main_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SalesAnalyticsMainPage extends StatefulWidget {
+  const SalesAnalyticsMainPage({super.key});
+
+  @override
+  State<SalesAnalyticsMainPage> createState() => _SalesAnalyticsMainPageState();
+}
+
+class _SalesAnalyticsMainPageState extends State<SalesAnalyticsMainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
